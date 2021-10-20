ba8d8ab301392a47a1c689f7adb51d357e018429
Mihir Dhruve <Mihir.Dhruve@allscripts.com>
2021-10-20 17:39:22 +0530

    Add project files.

---
+++ KafkaConsumer/Program.cs
@@ -0,0 +1,40 @@
+﻿using Confluent.Kafka;
+using System;
+
+namespace KafkaConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kafkaEndPoint = "localhost:9092";
+
+            string kafkaTopic = "TestTopic";
+
+            var consumerConfig = new ConsumerConfig
+            {
+                BootstrapServers = kafkaEndPoint,
+                GroupId= "MyGroup",                
+            };
+
+            using (var consumer = new ConsumerBuilder<Null, string>(consumerConfig).Build())
+            {
+                consumer.Subscribe(kafkaTopic);                
+                try
+                {
+                    while (true)
+                    {
+                        var consumeResult = consumer.Consume();
+                        Console.WriteLine(consumeResult.Message.Value);
+                    }
+                }
+                catch (Exception)
+                {
+                    consumer.Close();
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ KafkaSimple/Program.cs
@@ -0,0 +1,47 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Collections.Generic;
+
+namespace KafkaSimple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kafkaEndPoint = "localhost:9092";
+            string kafkaTopic = "TestTopic";
+            string input;
+
+            var producerConfig = new ProducerConfig()
+            {
+                BootstrapServers = kafkaEndPoint                
+            };
+
+
+            //Single input
+            //using (var producer = new ProducerBuilder<Null, string>(producerConfig).Build())
+            //{
+            //    producer.Flush();
+            //    producer.Produce(kafkaTopic, new Message<Null, string> { Value = $"Hello Allscripts" });
+            //}
+
+            // Continuously monitors input.        
+            while ((input = Console.ReadLine()) != null)
+            {
+                var cmd = input.Substring(0, input.Length);
+                switch (cmd)
+                {
+                    case "quit":
+                        return; // Stop the run thread
+                }
+
+                //Create producer
+                using (var producer = new ProducerBuilder<Null, string>(producerConfig).Build())
+                {
+                    Console.WriteLine($"Pushing message with value: {cmd} in topic: {kafkaTopic}");
+                    producer.Produce(kafkaTopic, new Message<Null, string> { Value = $"{cmd}" });
+                }
+            }            
+        }
+    }
+}
